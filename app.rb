96ea06f0f9710c77b141cc5da38ef8cc4651896e
github username <github email address>
2018-08-07 18:30:54 -0400

    This went relatively smoothly. Pay attention to those spec-tests, they're fickle

--- app.rb
+++ app.rb
@@ -2,4 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  get '/display_puppy' do
+    erb :display_puppy
+  end
+
+
+  post '/new' do
+    @puppy_text = Puppy.new(params[:name], params[:breed], params[:age])
+
+    erb :display_puppy
+  end
+
+
 end
